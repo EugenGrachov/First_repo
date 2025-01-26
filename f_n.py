5aaf3fe8936957ec5e7df8f4757a5277cdfff2a8
EugenGrachov <repkogithu@gmail.com>
2025-01-26 20:06:44 +0100

    matrix_multiplication

--- f_n.py
+++ f_n.py
@@ -31,3 +31,38 @@ a5 = np.matrix("1,2,3;0,1,2")
 b5 = np.matrix("1,2;0,4;-1,0")
 c5 = a5.dot(b5)
 print(c5)
+
+a22 = np.matrix("1,0,3;-1,-1,2;4,7,2")
+det = round(np.linalg.det(a22))
+print(det)
+
+a = np.matrix("1,0,3;-1,-1,2;4,7,2")
+a_inv = np.linalg.inv(a)
+print(a_inv)
+
+a = np.matrix("1,0,3;-1,-1,2;4,7,2")
+w, v = np.linalg.eig(a)
+print(w)
+print(v)
+
+a = np.matrix("1,2,5;1,-1,3;3,-6,-1")
+b = np.matrix("-9;2;25")
+a_inv = np.linalg.inv(a)
+x = a_inv.dot(b)
+print(x)
+
+a = np.matrix("1,2,5;1,-1,3;3,-6,-1")
+b = np.matrix("-9;2;25")
+a_det = round(np.linalg.det(a))
+a_1 = np.matrix(a)
+a_1[:, 0] = b
+a_2 = np.matrix(a)
+a_2[:, 1] = b
+a_3 = np.matrix(a)
+a_3[:, 2] = b
+x = [
+    round(np.linalg.det(a_1)) / a_det,
+    round(np.linalg.det(a_2)) / a_det,
+    round(np.linalg.det(a_3)) / a_det,
+]
+print(x)
