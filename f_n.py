6ecce922b6703ae5cc585d28c2f40740b2bcd7d9
EugenGrachov <repkogithu@gmail.com>
2025-01-26 17:19:03 +0100

    m_np

--- f_n.py
+++ f_n.py
@@ -19,3 +19,15 @@ a3 = np.matrix("1, 2, 3; 4, 5, 6")
 b2 = a3.reshape((3, 2))
 print(b2)
 
+a = np.matrix("1,2,3;4,5,6")
+b = a.reshape((3, 2))
+print(b)
+
+a = np.matrix([[1, 2, 3], [4, 5, 6]])
+b = a.flatten()
+print(b)
+
+a5 = np.matrix("1,2,3;0,1,2")
+b5 = np.matrix("1,2;0,4;-1,0")
+c5 = a5.dot(b5)
+print(c5)
